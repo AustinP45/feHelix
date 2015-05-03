7e62b9b80e799f56b425bc9be2742ede8aaedcb1
Austin <austinpeterson@outlook.com>
2015-05-03 17:04:02 -0500

    Added testing

--- ratings/tests.py
+++ ratings/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+def create_category(cat_name, cat_desc):
+	"""
+	Create a new category with given name and description
+	"""
+	return Category.models.create(name=cat_name, desc=cat_desc)
+
+class QuestionViewTests(TestCase):
+    def test_index_view(self):
+		"""
+        Ensure we get a response from the index
+        """
+        response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_categories_view(self):
+		"""
+        Ensure we get a response from the index
+        """
+		response = self.client.get('/categories')
+		self.assertEqual(response.status_code, 200)
+		
